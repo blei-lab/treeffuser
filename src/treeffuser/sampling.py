9d2d4e432cf208815488b1f556e371bfaf976aa0
aagrande <47125106+aagrande@users.noreply.github.com>
2024-02-15 13:23:57 -0500

    add initial predictor classes

--- src/treeffuser/sampling.py
+++ src/treeffuser/sampling.py
@@ -1,3 +1,101 @@
 """
-Something like here: https://github.com/yang-song/score_sde_pytorch/blob/main/sampling.py
+Abstract classes for sampling methods.
+Adapted from: http://tinyurl.com/torch-sde-lib-song
+The notice from the original code is as follows:
+
+ coding=utf-8
+ Copyright 2020 The Google Research Authors.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 """
+
+import abc
+
+import numpy as np
+import torch
+
+_PREDICTORS = {}
+
+
+def register_predictor(cls=None, *, name=None):
+    """A decorator for registering predictor classes."""
+
+    def _register(cls):
+        if name is None:
+            local_name = cls.__name__
+        else:
+            local_name = name
+        if local_name in _PREDICTORS:
+            raise ValueError(f"Already registered model with name: {local_name}")
+        _PREDICTORS[local_name] = cls
+        return cls
+
+    if cls is None:
+        return _register
+    else:
+        return _register(cls)
+
+
+def get_predictor(name):
+    return _PREDICTORS[name]
+
+
+class Predictor(abc.ABC):
+    """The abstract class for a predictor algorithm."""
+
+    def __init__(self, sde, score_fn, probability_flow=False):
+        super().__init__()
+        self.sde = sde
+        # Compute the reverse SDE/ODE
+        self.rsde = sde.reverse(score_fn, probability_flow)
+        self.score_fn = score_fn
+
+    @abc.abstractmethod
+    def update_fn(self, x, t):
+        """One update of the predictor.
+
+        Args:
+          x: A PyTorch tensor representing the current state
+          t: A Pytorch tensor representing the current time step.
+
+        Returns:
+          x: A PyTorch tensor of the next state.
+          x_mean: A PyTorch tensor. The next state without random noise. Useful for denoising.
+        """
+
+
+@register_predictor(name="euler_maruyama")
+class EulerMaruyamaPredictor(Predictor):
+    def __init__(self, sde, score_fn, probability_flow=False):
+        super().__init__(sde, score_fn, probability_flow)
+
+    def update_fn(self, x, t):
+        dt = -1.0 / self.rsde.N
+        z = torch.randn_like(x)
+        drift, diffusion = self.rsde.sde(x, t)
+        x_mean = x + drift * dt
+        x = x_mean + diffusion[:, None, None, None] * np.sqrt(-dt) * z
+        return x, x_mean
+
+
+@register_predictor(name="reverse_diffusion")
+class ReverseDiffusionPredictor(Predictor):
+    def __init__(self, sde, score_fn, probability_flow=False):
+        super().__init__(sde, score_fn, probability_flow)
+
+    def update_fn(self, x, t):
+        f, G = self.rsde.discretize(x, t)
+        z = torch.randn_like(x)
+        x_mean = x - f
+        x = x_mean + G[:, None, None, None] * z
+        return x, x_mean
