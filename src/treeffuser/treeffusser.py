Fixed incorrect sign

src/treeffuser/treeffusser.py
@@ -3,19 +3,23 @@ This should be the main file corresponding to the project.
 """
 
 import abc
+from typing import Optional
 
-import numpy as np
+from einops import rearrange
 from jaxtyping import Float
 from ml_collections import FrozenConfigDict
 from numpy import ndarray
 from sklearn.base import BaseEstimator
 
+import treeffuser._sampling as _sampling
+import treeffuser._score_models as _score_models
+import treeffuser._sdes as _sdes
 from treeffuser._preprocessors import Preprocessor
-from treeffuser._score_models import ScoreModel
+from treeffuser._score_models import Score
 
 
 def _check_arguments(
-    X: Float[ndarray, "batch x_dim"], y: Float[ndarray, "batch y_dim"]
+    X: Float[ndarray, "batch x_dim"], y: Float[ndarray, "batch y_dim"] = None
 ) -> None:
     """
     Check the arguments for the model.
@@ -33,24 +37,29 @@ class Treeffusser(BaseEstimator, abc.ABC):
     different parameters and methods.
     """
 
-    def __init__(self):
+    def __init__(self, sde_name: str = "vesde"):
         self._score_model = None
         self._is_fitted = False
         self._x_preprocessor = Preprocessor()
         self._y_preprocessor = Preprocessor()
+        self._y_dim = None
 
-    @abc.abstractmethod
+        # TODO: For the moment this is only supporting VESDEs
+        self._sde = _sdes.get_sde(sde_name)(sigma_max=4.0, sigma_min=0.01)
+        self._sde_name = sde_name
+
     @property
-    def _score_config(self) -> FrozenConfigDict:
+    @abc.abstractmethod
+    def score_config(self) -> FrozenConfigDict:
         """
         Should return the score config for the model.
         These are the parameters that will be used to initialize
         the score model.
         """
 
-    @abc.abstractmethod
     @property
-    def _score_model_class(self) -> ScoreModel:
+    @abc.abstractmethod
+    def _score_model_class(self) -> Score:
         """
         Should return the class of the score model.
         """
@@ -66,14 +75,24 @@ class Treeffusser(BaseEstimator, abc.ABC):
         x_transformed = self._x_preprocessor.fit_transform(X)
         y_transformed = self._y_preprocessor.fit_transform(y)
 
-        self._score_model = self.score_model_class(**self.score_config)
+        x_transformed = X
+        y_transformed = y
+
+        self._score_model = self._score_model_class(**self.score_config)
         self._score_model.fit(x_transformed, y_transformed)
 
+        self._y_dim = y.shape[1]
         self._is_fitted = True
         return self
 
     def sample(
-        self, X: Float[ndarray, "batch x_dim"], n_samples: int
+        self,
+        X: Float[ndarray, "batch x_dim"],
+        n_samples: int,
+        n_parallel: int = 100,
+        denoise: bool = False,
+        n_steps: int = 100,
+        seed=None,
     ) -> Float[ndarray, "batch n_samples y_dim"]:
         """
         Sample from the model.
@@ -81,98 +100,124 @@ class Treeffusser(BaseEstimator, abc.ABC):
         if not self._is_fitted:
             raise ValueError("The model has not been fitted yet.")
 
-        return np.zeros((X.shape[0], n_samples, X.shape[1]))
+        # x_transformed = self._x_preprocessor.transform(X)
+        x_transformed = X
+        # We don't have this infor since the beggining so we need to create
+        # the sde again
+        sde = _sdes.get_sde(self._sde_name)(N=100, sigma_min=0.01, sigma_max=4.0)
+
+        y_untransformed = _sampling.sample(
+            X=x_transformed,
+            y_dim=self._y_dim,
+            n_samples=n_samples,
+            score_fn=self._score_model.score,
+            n_parallel=n_parallel,
+            sde=sde,
+            n_steps=n_steps,
+            denoise=denoise,
+            seed=seed,
+            predictor_name="euler_maruyama",  # TODO: This should be a parameter
+            corrector_name="none",  # TODO: This should be a parameter
+            verbose=1,
+        )
+        y_untransformed = rearrange(
+            y_untransformed, "n_preds n_samples y_dim -> (n_preds n_samples) y_dim"
+        )
+        y_transformed = y_untransformed
+        y_transformed = self._y_preprocessor.inverse_transform(y_untransformed)
+        y_transformed = rearrange(
+            y_transformed,
+            "(n_preds n_samples) y_dim -> n_preds n_samples y_dim",
+            n_samples=n_samples,
+        )
+        return y_transformed
 
 
-# STILL NEEDS TO BE IMPLEMENTED
-#
-#
-# class Treeffuser(BaseEstimator):
-#
-#
-#    def __init__(self,
-#        likelihood_reweighting: Optional[bool] = False,
-#        n_repeats: Optional[int] = 1,
-#        n_estimators: Optional[int] = 100,
-#        eval_percent: Optional[float] = None,
-#        early_stopping_rounds: Optional[int] = None,
-#        num_leaves: Optional[int] = 31,
-#        max_depth: Optional[int] = -1,
-#        learning_rate: Optional[float] = 0.1,
-#        max_bin: Optional[int] = 255,
-#        subsample_for_bin: Optional[int] = 200000,
-#        min_child_samples: Optional[int] = 20,
-#        subsample: Optional[float] = 1.0,
-#        subsample_freq: Optional[int] = 0,
-#        verbose: Optional[int] = 0,
-#        seed: Optional[int] = 0,
-#        n_jobs: Optional[int] = -1,
-#    ):
-#        """
-#        Args:
-#        This model doesn't do any model checking or validation. It's assumed that
-#        that the main user is the `Treeffuser` class and that the user has already
-#        checked that the inputs are valid.
-#
-#        Diffusion model args
-#        -------------------------------
-#        likelihood_reweighting (bool): Whether to reweight the likelihoods.
-#        n_repeats (int): How many times to repeat the training dataset. i.e how
-#            many noisy versions of a point to generate for training.
-#
-#        LightGBM args
-#        -------------------------------
-#        eval_percent (float): Percentage of the training data to use for validation.
-#            If `None`, no validation set is used.
-#        early_stopping_rounds (int): If `None`, no early stopping is performed. Otherwise,
-#            the model will stop training if no improvement is observed in the validation
-#            set for `early_stopping_rounds` consecutive iterations.
-#        n_estimators (int): Number of boosting iterations.
-#        num_leaves (int): Maximum tree leaves for base learners.
-#        max_depth (int): Maximum tree depth for base learners, <=0 means no limit.
-#        learning_rate (float): Boosting learning rate.
-#        max_bin (int): Max number of bins that feature values will be bucketed in. This
-#            is used for lightgbm's histogram binning algorithm.
-#        subsample_for_bin (int): Number of samples for constructing bins (can ignore).
-#        min_child_samples (int): Minimum number of data needed in a child (leaf). If
-#            less than this number, will not create the child.
-#        subsample (float): Subsample ratio of the training instance.
-#        subsample_freq (int): Frequence of subsample, <=0 means no enable.
-#            How often to subsample the training data.
-#        seed (int): Random seed.
-#        early_stopping_rounds (int): If `None`, no early stopping is performed. Otherwise,
-#            the model will stop training if no improvement is observed in the validation
-#        n_jobs (int): Number of parallel threads. If set to -1, the number is set to the
-#            number of available cores.
-#        """
-#        # Stuff related to the diffusion model
-#        self.score_config =
-#
-#
-#    def fit(self, X: Float[ndarray, "batch x_dim"], y: Float[ndarray, "batch y_dim"]):
-#        """
-#        Fit the model to the data.
-#
-#        Returns an instance of the model for chaining.
-#        """
-#        _check_arguments(X, y)
-#        pass
-#
-#
-#    def predict(self, X):
-#        pass
-#
-#    def sample(self, X):
-#        pass
-#
-#    def likelihood(self, X, y):
-#        """
-#        Something that computes the log-likelihood of the model.
-#        """
-#        pass
-#
-#    def pred_distribution(self, X):
-#        """
-#        Maybe the CDF?
-#        """
-#        pass
+class LightGBMTreeffusser(Treeffusser):
+    def __init__(
+        self,
+        # Diffusion model args
+        sde_name: Optional[str] = "vesde",
+        # Score estimator args
+        likelihood_reweighting: Optional[bool] = False,
+        n_repeats: Optional[int] = 10,
+        n_estimators: Optional[int] = 100,
+        eval_percent: Optional[float] = None,
+        early_stopping_rounds: Optional[int] = None,
+        num_leaves: Optional[int] = 31,
+        max_depth: Optional[int] = -1,
+        learning_rate: Optional[float] = 0.1,
+        max_bin: Optional[int] = 255,
+        subsample_for_bin: Optional[int] = 200000,
+        min_child_samples: Optional[int] = 20,
+        subsample: Optional[float] = 1.0,
+        subsample_freq: Optional[int] = 0,
+        verbose: Optional[int] = 0,
+        seed: Optional[int] = 0,
+        n_jobs: Optional[int] = -1,
+    ):
+        """
+        Args:
+        This model doesn't do any model checking or validation. It's assumed that
+        that the main user is the `Treeffuser` class and that the user has already
+        checked that the inputs are valid.
+        Diffusion model args
+        -------------------------------
+        likelihood_reweighting (bool): Whether to reweight the likelihoods.
+        n_repeats (int): How many times to repeat the training dataset. i.e how
+         many noisy versions of a point to generate for training.
+        LightGBM args
+        -------------------------------
+        eval_percent (float): Percentage of the training data to use for validation.
+            If `None`, no validation set is used.
+        early_stopping_rounds (int): If `None`, no early stopping is performed. Otherwise,
+            the model will stop training if no improvement is observed in the validation
+            set for `early_stopping_rounds` consecutive iterations.
+        n_estimators (int): Number of boosting iterations.
+        num_leaves (int): Maximum tree leaves for base learners.
+        max_depth (int): Maximum tree depth for base learners, <=0 means no limit.
+        learning_rate (float): Boosting learning rate.
+        max_bin (int): Max number of bins that feature values will be bucketed in. This
+            is used for lightgbm's histogram binning algorithm.
+        subsample_for_bin (int): Number of samples for constructing bins (can ignore).
+        min_child_samples (int): Minimum number of data needed in a child (leaf). If
+            less than this number, will not create the child.
+        subsample (float): Subsample ratio of the training instance.
+        subsample_freq (int): Frequence of subsample, <=0 means no enable.
+            How often to subsample the training data.
+        seed (int): Random seed.
+        early_stopping_rounds (int): If `None`, no early stopping is performed. Otherwise,
+            the model will stop training if no improvement is observed in the validation
+        n_jobs (int): Number of parallel threads. If set to -1, the number is set to the
+            number of available cores.
+        """
+        super().__init__(sde_name=sde_name)
+        self._score_config = FrozenConfigDict(
+            {
+                "sde": self._sde,
+                "likelihood_reweighting": likelihood_reweighting,
+                "n_repeats": n_repeats,
+                "n_estimators": n_estimators,
+                "eval_percent": eval_percent,
+                "early_stopping_rounds": early_stopping_rounds,
+                "num_leaves": num_leaves,
+                "max_depth": max_depth,
+                "learning_rate": learning_rate,
+                "max_bin": max_bin,
+                "subsample_for_bin": subsample_for_bin,
+                "min_child_samples": min_child_samples,
+                "subsample": subsample,
+                "subsample_freq": subsample_freq,
+                "verbose": verbose,
+                "seed": seed,
+                "n_jobs": n_jobs,
+            }
+        )
+
+    @property
+    def score_config(self):
+        return self._score_config
+
+    @property
+    def _score_model_class(self):
+        return _score_models.LightGBMScore

tests/test_treeffuser.py
@@ -1,2 +1,38 @@
-def test_main():
-    pass
+import numpy as np
+from matplotlib import pyplot as plt
+
+from treeffuser import LightGBMTreeffusser
+from utils import generate_bimodal_linear_regression_data
+
+
+def test_treeffuser_bimodal_linear_regression():
+
+    n = 1000
+    p = 1
+    sigma = 0.1
+    n_samples = 10
+
+    X, y = generate_bimodal_linear_regression_data(n, p, sigma, bimodal=True, seed=0)
+
+    model = LightGBMTreeffusser(
+        verbose=1,
+        n_repeats=100,
+        n_estimators=200,
+        likelihood_reweighting=True,
+        sde_name="vesde",
+        learning_rate=0.01,
+    )
+    model.fit(X, y)
+
+    # shape (n , n_samples, y_dim)
+    y_samples = model.sample(
+        X, n_samples=n_samples, n_parallel=100, denoise=False, n_steps=100, seed=0
+    )
+    X_repeated = np.repeat(X, n_samples, axis=0)
+
+    plt.scatter(X_repeated, y_samples, alpha=0.1, color="blue", label="Samples")
+    plt.scatter(X, y, alpha=0.1, color="red", label="Data")
+    plt.show()
+
+
+test_treeffuser_bimodal_linear_regression()

tests/utils.py
@@ -31,7 +31,9 @@ def generate_bimodal_linear_regression_data(
     X = np.random.normal(size=(n, p))
     if bimodal:
         y = np.random.choice([0, 1], size=n)
-        y = y * (X @ np.random.normal(size=p)) + (1 - y) * (X @ np.random.normal(size=p))
+        y = y * (X @ np.random.normal(size=p) * 2) + (1 - y) * (
+            X @ np.random.normal(size=p) * 2
+        )
     else:
         y = X @ np.random.normal(size=p)
     y += np.random.normal(size=n) * sigma