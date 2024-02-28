79b59f069fbd7cfe044c7c520963476acd91fe46
Nicolas Beltran <nb2838@columbia.edu>
2024-02-28 18:53:43 -0500

    fixed some tests and increased test speed

--- tests/test_score_models.py
+++ tests/test_score_models.py
@@ -3,7 +3,6 @@ Contains all of the test for the different score model classes.
 """
 
 import numpy as np
-import pytest
 from einops import repeat
 from sklearn.metrics import r2_score
 
@@ -67,16 +66,15 @@ def test_linear_regression():
     assert r2 > 0.95, f"R^2 is {r2}"
 
 
-@pytest.mark.parametrize("seed", [None, 0])
-def test_is_deterministic(seed):
+def test_can_be_deterministic():
     # Params
-    n = 1000
+    n = 200
     x_dim = 1
     y_dim = 1
     sigma = 0.00001
-    n_estimators = 100
-    learning_rate = 0.01
-    n_repeats = 10
+    n_estimators = 50
+    learning_rate = 0.1
+    n_repeats = 1
 
     X, y = generate_bimodal_linear_regression_data(n, x_dim, sigma, bimodal=False, seed=0)
 
@@ -87,6 +85,7 @@ def test_is_deterministic(seed):
     sigma_min = 0.01
     sigma_max = y.std()
     sde = VESDE(sigma_min=sigma_min, sigma_max=sigma_max)
+    seed = 0
 
     # First fit
     score_model_a = LightGBMScore(
@@ -122,11 +121,67 @@ def test_is_deterministic(seed):
     scores_a = score_model_a.score(y=y_perturbed, X=X, t=random_t)
     scores_b = score_model_b.score(y=y_perturbed, X=X, t=random_t)
 
-    if seed is None:
-        # Check that the score model gives different results
-        msg = "The score model is deterministic when it shouldn't be"
-        assert not np.allclose(scores_a, scores_b), msg
-    else:
-        # Check that the score model gives the same results
-        msg = "The score model is not deterministic"
-        assert np.allclose(scores_a, scores_b), msg
+    msg = "The score model is not deterministic"
+    assert np.allclose(scores_a, scores_b), msg
+
+
+def test_different_seeds_do_not_give_same_results():
+    # Params
+    n = 200
+    x_dim = 1
+    y_dim = 1
+    sigma = 0.00001
+    n_estimators = 50
+    learning_rate = 0.1
+    n_repeats = 5
+
+    X, y = generate_bimodal_linear_regression_data(n, x_dim, sigma, bimodal=False, seed=0)
+
+    assert X.shape == (n, x_dim)
+    assert y.shape == (n, y_dim)
+
+    # Fit a score model
+    sigma_min = 0.01
+    sigma_max = y.std()
+    sde = VESDE(sigma_min=sigma_min, sigma_max=sigma_max)
+
+    seed_a = 0
+    seed_b = 1
+
+    # First fit
+    score_model_a = LightGBMScore(
+        sde=sde,
+        verbose=1,
+        n_estimators=n_estimators,
+        learning_rate=learning_rate,
+        n_repeats=n_repeats,
+        seed=seed_a,
+    )
+    score_model_a.fit(X, y)
+
+    # Second fit
+    score_model_b = LightGBMScore(
+        sde=sde,
+        verbose=1,
+        n_estimators=n_estimators,
+        learning_rate=learning_rate,
+        n_repeats=n_repeats,
+        seed=seed_b,
+    )
+    score_model_b.fit(X, y)
+
+    # Check that the two results are the same
+    random_t = np.random.uniform(0, sde.T // 2, size=n)
+    random_t = repeat(random_t, "n -> n 1")
+    z = np.random.randn(n)
+    z = repeat(z, "n -> n y_dim", y_dim=y_dim)
+
+    mean, std = sde.marginal_prob(y, random_t)
+    y_perturbed = mean + z * std
+
+    scores_a = score_model_a.score(y=y_perturbed, X=X, t=random_t)
+    scores_b = score_model_b.score(y=y_perturbed, X=X, t=random_t)
+
+    # Check that the score model gives different results
+    msg = "The score model gives the same results for different seeds"
+    assert not np.allclose(scores_a, scores_b), msg
