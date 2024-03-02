6836266c3fb260f551f89806576fa0907b179216
aagrande <47125106+aagrande@users.noreply.github.com>
2024-03-01 20:24:12 -0500

    add UCI wine quality dataset

---
+++ data/uci/wine/preprocess.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def main():
+    # import original datasets
+    red = np.genfromtxt("original/winequality-red.csv", delimiter=";", skip_header=True)
+    white = np.genfromtxt("original/winequality-white.csv", delimiter=";", skip_header=True)
+
+    # concatenate them
+    x = np.concatenate((red, white), axis=0)
+
+    # add covariate for red vs. white
+    red_color = np.array([1] * red.shape[0] + [0] * white.shape[0], dtype=np.float64).reshape(
+        (-1, 1)
+    )
+    x = np.concatenate((x, red_color), axis=1)
+
+    # extract outcome and covariates
+    y = x[:, -2].copy()
+    x = np.delete(x, -2, 1)
+    categorical = [x.shape[1]]
+
+    np.save("data.npy", {"x": x, "y": y, "categorical": categorical})
+
+
+if __name__ == "__main__":
+    main()
---
+++ data/uci/wine/readme.md
@@ -0,0 +1,11 @@
+# Wine (Regression)
+* Download the following files from the [UCI Repository: Wine](https://archive.ics.uci.edu/ml/datasets/Wine+Quality) to `./original/`.
+    * `winequality-red.csv`.
+    * `winequality-white.csv`.
+
+* Preprocess the data.
+    * Run `python3 preprocess.py`.
+    	* This creates a `data.npy`.
+
+## Acknowledgments
+This README is adapted from [ibug](https://github.com/jjbrophy47/ibug/tree/master/data).
