d2644a234b10a0d1d9e18a9f885758376a8650fb
achille <achille.nazaret@gmail.com>
2024-03-08 20:05:16 -0500

    add base_sde.py

---
+++ src/treeffuser/sde/base_sde.py
@@ -0,0 +1,103 @@
+import abc
+from typing import Callable
+
+from jaxtyping import Float
+from numpy import ndarray
+
+_AVAILABLE_SDES = {}
+
+
+def _register_sde(name):
+    """A decorator for registering available SDE."""
+
+    def _register(cls):
+        if name in _AVAILABLE_SDES:
+            raise ValueError(f"Already registered SDE with name: {name}")
+        _AVAILABLE_SDES[name] = cls
+        return cls
+
+    return _register
+
+
+def get_sde(name):
+    """Get an SDE by name."""
+    if name not in _AVAILABLE_SDES:
+        raise ValueError(f"Unknown SDE {name}. Available SDEs: {list(_AVAILABLE_SDES.keys())}")
+    return _AVAILABLE_SDES[name]
+
+
+class BaseSDE(abc.ABC):
+    """
+    Abstract class representing a stochastic differential equation (SDE):
+    `dY = f(Y, t) dt + g(Y, t) dW` where `Y` is the state, `t` is time,
+    `f` is the drift and `g` is the diffusion [1].
+
+    BaseSDE must implement:
+    - `drift_and_diffusion(y, t)`, which return a tuple of the drift and the
+    diffusion at time `t` when `Y=y`.
+
+    References:
+    -----------
+    [1] https://en.wikipedia.org/wiki/Stochastic_differential_equation
+    """
+
+    @abc.abstractmethod
+    def drift_and_diffusion(
+        self, y: Float[ndarray, "batch y_dim"], t: Float[ndarray, "batch"]
+    ) -> (Float[ndarray, "batch y_dim"], Float[ndarray, "batch y_dim"]):
+        """
+        Drift and diffusion of the SDE at time `t` and value `y`.
+        """
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
+
+
+class ReverseSDE(BaseSDE):
+    """
+    ReverseSDE, is the SDE reversed in time. To be reversed, SDEs require a
+    transformation of the drift term, based on the score function of the marginal
+    distributions induced by the original SDE [1].
+
+    The original SDE `dY = f(Y, t) dt + g(Y, t) dW` can be reversed from
+    time `T` to define a new SDE:
+    `dY(T-t) = (-f(Y, T-t) + g(Y, T-t)² ∇[log p(Y(T-t))]) dt + g(Y, T-t) dW`
+
+    Parameters:
+    -----------
+    sde : BaseSDE
+        The original SDE.
+    t_origin : float
+        The time from which to reverse the SDE.
+    score_fn : Callable[[Float[ndarray, "batch y_dim"], Float[ndarray, "batch"]], Float[ndarray, "batch"]]
+        The score function of the original SDE induced marginal distributions.
+
+    References:
+    -----------
+    [1] https://openreview.net/pdf?id=PxTIG12RRHS
+
+    """
+
+    def __init__(
+        self,
+        sde: BaseSDE,
+        t_origin: float,
+        score_fn: Callable[
+            [Float[ndarray, "batch y_dim"], Float[ndarray, "batch"]], Float[ndarray, "batch"]
+        ],
+    ):
+        self.sde = sde
+        self.t_origin = t_origin
+        self.score_fn = score_fn
+
+    def drift_and_diffusion(
+        self, y: Float[ndarray, "batch y_dim"], t: Float[ndarray, "batch"]
+    ) -> (Float[ndarray, "batch y_dim"], Float[ndarray, "batch y_dim"]):
+        drift, diffusion = self.sde.drift_and_diffusion(y, self.t_origin - t)
+        drift = -drift + diffusion**2 * self.score_fn(y, self.t_origin - t)
+        return drift, diffusion
+
+    def __repr__(self):
+        return (
+            f"ReverseSDE(sde={self.sde}, t_origin={self.t_origin}, score_fn={self.score_fn})"
+        )
