e1b7843e0b775f81d1a6fb8c4f62748cadc6caf3
aagrande <47125106+aagrande@users.noreply.github.com>
2024-03-09 23:34:53 -0500

    fix nasty unzip for UCI naval dataset

--- testbed/data/uci/naval/preprocess.py
+++ testbed/data/uci/naval/preprocess.py
@@ -1,16 +1,26 @@
 import argparse
 import zipfile
 from pathlib import Path
+from shutil import rmtree  # delete non-empty folders
 
 import numpy as np
 
 
 def main(path_raw_dataset_dir: Path):
-    # unzip and delete original raw files
+    # unzip and delte original raw files
     path_raw_data_file = path_raw_dataset_dir / "temp.zip"
-    with zipfile.ZipFile(path_raw_data_file, "r") as raw_zip:
-        raw_zip.extractall(path_raw_dataset_dir)
+    with zipfile.ZipFile(path_raw_data_file, "r") as archive:
+        for file in archive.namelist():
+            if file.startswith("UCI CBM Dataset/"):
+                archive.extract(file, path_raw_dataset_dir)
     path_raw_data_file.unlink()
+
+    # delete "UCI CBM Dataset" folder after moving raw files
+    path_uci_cbm_dir = path_raw_dataset_dir / "UCI CBM Dataset"
+    for file in path_uci_cbm_dir.iterdir():
+        if file.suffix == ".txt":
+            file.rename(path_raw_dataset_dir / file.name)
+    rmtree(path_uci_cbm_dir)
 
     # extract outcome and covariates
     x = np.genfromtxt(path_raw_dataset_dir / "data.txt", delimiter="   ", skip_header=False)
