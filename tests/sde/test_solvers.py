2e77716e3effcda22fcd368a8b29c03f6090bb7e
achille <achille.nazaret@gmail.com>
2024-03-11 10:40:11 -0400

    add tests.

---
+++ tests/sde/test_solvers.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from treeffuser.sde import sdeint
+from treeffuser.sde.base_sde import CustomSDE
+
+
+def test_euler_maruyama():
+    # Samples from this SDE are ~ Normal(y, t)
+    drift = lambda y, t: 0
+    diffusion = lambda y, t: 1
+    sde = CustomSDE(drift, diffusion)
+
+    batch_size = 1000
+    y0 = np.zeros((batch_size, 1))
+    samples = sdeint(sde, y0, 0, 1, n_steps=20, method="euler", seed=0)
+    assert samples.shape == (batch_size, 1)
+    samples = sdeint(sde, y0, 0, 1, n_steps=20, n_samples=3, method="euler", seed=0)
+    assert samples.shape == (3, batch_size, 1)
+
+    for t in [0.5, 1, 2]:
+        samples = sdeint(sde, y0, 0, t, n_steps=20, method="euler", seed=0)
+        # Mean should be close to 0.
+        assert np.abs(samples.mean()) < 0.1
+        # Variance should be close to t.
+        assert np.abs(samples.var() - t) < 0.1
