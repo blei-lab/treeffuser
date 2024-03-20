594c41c5ba3a03169315abd77093c7f28daada2d
Nicolas Beltran <nb2838@columbia.edu>
2024-03-20 15:56:30 -0400

    Added a distribution class and changed base probabilistic model class

---
+++ testbed/src/testbed/distributions/distribution.py
@@ -0,0 +1,50 @@
+from abc import ABC
+from abc import abstractmethod
+
+from jaxtyping import Float
+from numpy import ndarray
+
+
+class Distribution(ABC):
+    """
+    Represents a probability distribution of the form p(y | x).
+    where x has shape (batch, x_dim) and y has shape (batch, y_dim).
+
+    Each method should implement its own distribution function.
+    """
+
+    @abstractmethod
+    def sample(n_samples: int) -> Float[ndarray, "n_samples batch y_dim"]:
+        """
+        Sample from the probability distribution for each input.
+        """
+
+    @abstractmethod
+    def log_likelihood(self, y: Float[ndarray, "batch y_dim"]) -> Float[ndarray, "batch"]:
+        """
+        Compute the log likelihood of the data under the model.
+        """
+
+    @property
+    @abstractmethod
+    def x_dim(self) -> int:
+        """
+        The dimension of the input.
+        """
+
+    @property
+    @abstractmethod
+    def y_dim(self) -> int:
+        """
+        The dimension of the output.
+        """
+
+    @property
+    @abstractmethod
+    def batch_size(self) -> int:
+        """
+        The batch size of the distribution.
+        """
+
+    def __init__(self):
+        super().__init__()
--- testbed/src/testbed/models/base_model.py
+++ testbed/src/testbed/models/base_model.py
@@ -2,20 +2,27 @@ from abc import ABC
 from abc import abstractmethod
 
 from jaxtyping import Float
+from jaxtyping import List
 from numpy import ndarray
+from sklearn.base import BaseEstimator
 
 
-class ProbabilisticModel(ABC):
+class ProbabilisticModel(ABC, BaseEstimator):
     """
-    A base class for all probabilistic models. Which produce a probability distribution
-    rather than a single output for each input.
+    A base class for all probabilistic models. Which produces a probability distribution
+    rather than a single output for each input. Subclasses BaseEstimator.
     """
 
     def __init__(self):
         super().__init__()
 
     @abstractmethod
-    def fit(self, X: Float[ndarray, "batch x_dim"], y: Float[ndarray, "batch y_dim"]):
+    def fit(
+        self,
+        X: Float[ndarray, "batch x_dim"],
+        y: Float[ndarray, "batch y_dim"],
+        cat_idx: List[int] = None,
+    ) -> None:
         """
         Fit the model to the data.
         """
@@ -23,21 +30,13 @@ class ProbabilisticModel(ABC):
     @abstractmethod
     def predict(self, X: Float[ndarray, "batch x_dim"]) -> Float[ndarray, "batch y_dim"]:
         """
+        Predict the mean for each input.
+        """
+
+    @abstractmethod
+    def predict_distribution(
+        self, X: Float[ndarray, "batch x_dim"]
+    ) -> Float[ndarray, "batch y_dim"]:
+        """
         Predict the probability distribution for each input.
-        """
-
-    @abstractmethod
-    def sample(
-        self, X: Float[ndarray, "batch x_dim"], n_samples: int
-    ) -> Float[ndarray, "n_samples batch y_dim"]:
-        """
-        Sample from the probability distribution for each input.
-        """
-
-    @abstractmethod
-    def log_likelihood(
-        self, X: Float[ndarray, "batch x_dim"], y: Float[ndarray, "batch y_dim"]
-    ) -> Float[ndarray, "batch"]:
-        """
-        Compute the log likelihood of the data under the model.
         """
