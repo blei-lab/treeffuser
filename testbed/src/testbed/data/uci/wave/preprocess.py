1ba2db3f7521750e4c577b4d8f232a213156f99b
aagrande <47125106+aagrande@users.noreply.github.com>
2024-04-22 17:42:56 -0400

    add preprocessing script for wave dataset

---
+++ testbed/src/testbed/data/uci/wave/preprocess.py
@@ -0,0 +1,40 @@
+import argparse
+import zipfile
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+
+def main(path_raw_dataset_dir: Path):
+    # unzip and delte original raw files
+    path_raw_data_file = path_raw_dataset_dir / "temp.zip"
+    with zipfile.ZipFile(path_raw_data_file, "r") as archive:
+        for file in archive.namelist():
+            archive.extract(file, path_raw_dataset_dir)
+    path_raw_data_file.unlink()
+
+    # import data
+    data = {}
+    path_raw_dataset_dir_WEC = path_raw_dataset_dir / "WECs_DataSet"
+    for city in ["Adelaide", "Perth", "Sydney", "Tasmania"]:
+        data[city] = pd.read_csv(path_raw_dataset_dir_WEC / (city + "_Data.csv"), header=None)
+    data = pd.concat([data[city] for city in data])
+
+    # extract outcome and covariates
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+    categorical = []
+
+    # save preprocessed data
+    np.save(
+        path_raw_dataset_dir.parent / "data.npy",
+        {"x": X.values, "y": y.values, "categorical": categorical},
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=Path)
+    args = parser.parse_args()
+    main(args.path)
