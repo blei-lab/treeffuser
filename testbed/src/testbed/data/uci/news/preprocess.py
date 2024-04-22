99c9ea8b1f8234fad280c7306f901af163219995
aagrande <47125106+aagrande@users.noreply.github.com>
2024-04-22 15:28:22 -0400

    add preprocessing script for news dataset

---
+++ testbed/src/testbed/data/uci/news/preprocess.py
@@ -0,0 +1,38 @@
+import argparse
+import zipfile
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+
+def main(path_raw_dataset_dir: Path):
+    # unzip and delte original raw files
+    path_raw_data_file = path_raw_dataset_dir / "temp.zip"
+    with zipfile.ZipFile(path_raw_data_file, "r") as archive:
+        for file in archive.namelist():
+            archive.extract(file, path_raw_dataset_dir)
+    path_raw_data_file.unlink()
+
+    # import data
+    data = pd.read_csv(
+        path_raw_dataset_dir / "OnlineNewsPopularity/OnlineNewsPopularity.csv", header=None
+    )
+
+    # extract outcome and covariates
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+    categorical = [0]
+
+    # save preprocessed data
+    np.save(
+        path_raw_dataset_dir.parent / "data.npy",
+        {"x": X.values, "y": y.values, "categorical": categorical},
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=Path)
+    args = parser.parse_args()
+    main(args.path)
