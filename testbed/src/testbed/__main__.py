2b8c6fc9f4213eb8e2250cd200f35a0fae182500
achille <achille.nazaret@gmail.com>
2024-04-23 22:33:57 -0400

    refactor the main test script

--- testbed/src/testbed/__main__.py
+++ testbed/src/testbed/__main__.py
@@ -2,6 +2,9 @@ import argparse
 import logging
 from typing import Dict
 from typing import List
+from typing import Literal
+from typing import Optional
+from typing import Type
 
 import namesgenerator
 import pandas as pd
@@ -14,35 +17,84 @@ from testbed.data.utils import list_data
 from testbed.metrics import AccuracyMetric
 from testbed.metrics import LogLikelihoodFromSamplesMetric
 from testbed.metrics import Metric
-
-# from testbed.metrics import QuantileCalibrationErrorMetric
+from testbed.metrics import QuantileCalibrationErrorMetric
 from testbed.models.base_model import BayesOptProbabilisticModel
-from testbed.models.ngboost import NGBoostGaussian
-from testbed.models.ngboost import NGBoostMixtureGaussian
+from testbed.models.base_model import ProbabilisticModel
 
 logger = logging.getLogger(__name__)
+
+
+def get_model(
+    model_name: Optional[str] = None, return_available_models: bool = False
+) -> List[str] | Type[ProbabilisticModel]:
+    # noinspection PyListCreation
+    available_models = []
+
+    available_models.append("ngboost")
+    if model_name == "ngboost":
+        from testbed.models.ngboost import NGBoostGaussian
+
+        return NGBoostGaussian
+
+    available_models.append("ngboost_mixture_gaussian")
+    if model_name == "ngboost_mixture_gaussian":
+        from testbed.models.ngboost import NGBoostMixtureGaussian
+
+        return NGBoostMixtureGaussian
+
+    available_models.append("treeffuser")
+    if model_name == "treeffuser":
+        from testbed.models.treeffuser import Treeffuser
+
+        return Treeffuser
+
+    available_models.append("card")
+    if model_name == "card":
+        from testbed.models.lightning_uq_models import Card
+
+        return Card
+    available_models.append("deep_ensemble")
+    if model_name == "deep_ensemble":
+        from testbed.models.lightning_uq_models import DeepEnsemble
+
+        return DeepEnsemble
+
+    available_models.append("mc_dropout")
+    if model_name == "mc_dropout":
+        from testbed.models.lightning_uq_models import MCDropout
+
+        return MCDropout
+
+    available_models.append("quantile_regression")
+    if model_name == "quantile_regression":
+        from testbed.models.lightning_uq_models import QuantileRegression
+
+        return QuantileRegression
+
+    available_models.append("ibug")
+    if model_name == "ibug":
+        from testbed.models.ibug_ import IBugXGBoost
+
+        return IBugXGBoost
+
+    if return_available_models:
+        return available_models
+
+    raise ValueError(
+        f"Model {model_name} is not available. Available models: {available_models}"
+    )
+
 
 ###########################################################
 #                 CONSTANTS                               #
 ###########################################################
 
 AVAILABLE_DATASETS = list(list_data().keys())
-
-
-# Treeffuser is not imported by default as it causes a segmentation fault
-# of Card model. If adequate it gets added to this dictionary.
-# by the proc_model_names function.
-MODEL_TO_CLASS = {
-    "ngboost_gaussian": NGBoostGaussian,
-    "ngboost_mixture_gaussian": NGBoostMixtureGaussian,
-    "card": None,
-    "treeffuser": None,
-}
-AVAILABLE_MODELS = list(MODEL_TO_CLASS.keys())
+AVAILABLE_MODELS = get_model(return_available_models=True)
 
 METRIC_TO_CLASS = {
     "accuracy": AccuracyMetric,
-    # "quantile_calibration_error": QuantileCalibrationErrorMetric,
+    "quantile_calibration_error": QuantileCalibrationErrorMetric,
     "log_likelihood": LogLikelihoodFromSamplesMetric,
 }
 AVAILABLE_METRICS = list(METRIC_TO_CLASS.keys())
@@ -70,38 +122,6 @@ def lst_to_new_line(lst: list) -> str:
     for item in lst:
         string += f"- {item}\n"
     return string
-
-
-def update_metric_to_class(models: List[str]):
-    """
-    Adds Treeffuser to MODEL_TO_CLASS if it is in the list of models.
-    Changes global variable MODEL_TO_CLASS.
-
-    There is an odd bug such that if Treeffuser is imported then Card
-    doesn't run and we get a segmentation fault. We solve this by
-    a conditional import statement (unfortunately).
-
-    This function verifies that we are not asked to run both Treeffuser
-    and Card models together and imports Treeffuser if needed.
-
-    This is extremely hacky and should be fixed in the future.
-
-    Args:
-        models (List[str]): List of models to run.
-    """
-
-    if "treeffuser" in models and "card" in models:
-        msg = "Treeffuser and Card models can't be run together. Segmentation fault occurs."
-        raise ValueError(msg)
-
-    if "treeffuser" in models:
-        from testbed.models.treeffuser import Treeffuser
-
-        MODEL_TO_CLASS["treeffuser"] = Treeffuser
-    if "card" in models:
-        from testbed.models.card import Card
-
-        MODEL_TO_CLASS["card"] = Card
 
 
 def parse_args():
@@ -155,19 +175,39 @@ def parse_args():
         help=msg,
     )
 
-    msg = "Whether to optimize the hyperparameters of the models."
+    msg = "Mode of model evaluation."
+    msg += "cross_val: evaluate the split in --split_idx with the default parameters"
+    msg += " or bayes_opt: optimize the hyperparameters with bayesian optimization on a single split."
+    msg += " Default: cross_val."
     parser.add_argument(
-        "--optimize_hyperparameters",
-        action="store_true",
+        "--evaluation_mode",
+        type=str,
         help=msg,
+        default="cross_val",
     )
 
     msg = "Number of iterations for the Bayesian optimization. To use"
-    msg += " this option, the --optimize_hyperparameters flag must be set."
+    msg += " this option, set --evaluation_mode bayes_opt."
     parser.add_argument(
         "--n_iter_bayes_opt",
         type=int,
         default=20,
+        help=msg,
+    )
+
+    msg = "Which split to evaluate the model on. Default: 0. To use"
+    msg += " this option, set --evaluation_mode cross_val."
+    parser.add_argument(
+        "--split_idx",
+        type=int,
+        default=0,
+        help=msg,
+    )
+
+    msg = "Wandb project name. Disable wandb logging if not provided."
+    parser.add_argument(
+        "--wandb_project",
+        type=str,
         help=msg,
     )
 
@@ -187,6 +227,12 @@ def check_args(args):
             msg = f"Model {model_name} is not available."
             msg += f" Available models: {lst_to_new_line(AVAILABLE_MODELS)}"
 
+    # There is an odd bug such that Card gets a segmentation fault if Treeffuser was imported.
+    # Hence, we cannot use both of them in the same run.
+    if "card" in args.models and "treeffuser" in args.models:
+        msg = "Card and Treeffuser cannot be run in the same script."
+        raise ValueError(msg)
+
     # check dataset name is valid
     for dataset_name in args.datasets:
         if dataset_name not in AVAILABLE_DATASETS:
@@ -205,6 +251,12 @@ def check_args(args):
     if args.n_iter_bayes_opt <= 0:
         msg = "The number of iterations for the Bayesian optimization must be positive."
         raise ValueError(msg)
+
+    # check that split_idx is in [0, 9] if evaluation_mode is cross_val
+    if args.evaluation_mode == "cross_val":
+        if args.split_idx < 0 or args.split_idx > 9:
+            msg = "The split index must be in [0, 9] if evaluation_mode is cross_val."
+            raise ValueError(msg)
 
 
 def format_results(model_name: str, dataset_name: str, results: Dict[str, float]) -> str:
@@ -254,8 +306,9 @@ def run_model_on_dataset(
     y_test: Float[ndarray, "test_size 1"],
     model_name: str,
     metrics: List[Metric],
-    optimize_hyperparameters: bool,
+    evaluation_mode: Literal["bayes_opt", "cross_val"] = "cross_val",
     n_iter_bayes_opt: int = 20,
+    seed: int = 0,
 ) -> Dict[str, float]:
     """
     Run a model on a dataset and compute the metrics specified.
@@ -272,13 +325,13 @@ def run_model_on_dataset(
     Returns:
         Dict[str, float]: Results of the model on the dataset.
     """
-    model_class = MODEL_TO_CLASS[model_name]
-    if optimize_hyperparameters:
+    model_class = get_model(model_name)
+    if evaluation_mode == "bayes_opt":
         model = BayesOptProbabilisticModel(
             model_class=model_class, n_iter_bayes_opt=n_iter_bayes_opt, cv=4, n_jobs=1
         )
     else:
-        model = model_class()
+        model = model_class(seed=seed)
 
     model.fit(X_train, y_train)
 
@@ -288,11 +341,7 @@ def run_model_on_dataset(
         res = metric.compute(model=model, X_test=X_test, y_test=y_test)
         results.update(res)
 
-    if optimize_hyperparameters:
-        results.update(model._model.get_params())
-        results["n_iter_bayes_opt"] = n_iter_bayes_opt
-    else:
-        results.update(model.get_params())
+    results.update(model.get_params())
     return results
 
 
@@ -305,22 +354,27 @@ def main() -> None:
     args = parse_args()
     check_args(args)
 
-    # This line should be added before everything else
-    # see the docstring of the function for more information.
-    update_metric_to_class(args.models)
-
     run_name = namesgenerator.get_random_name()
     full_results = []
 
     header = format_header(args, run_name)
     logger.info(header)
 
+    # setup wandb
+
     for model_name in args.models:
         for dataset_name in args.datasets:
             data = get_data(dataset_name, verbose=True)
-            X_train, X_test, y_train, y_test = train_test_split(
-                data["x"], data["y"], test_size=0.2, random_state=args.seed
-            )
+
+            if args.evaluation_mode == "cross_val":
+                X_train = data["x"][data["k_fold_splits"] != args.split_idx]
+                y_train = data["y"][data["k_fold_splits"] != args.split_idx]
+                X_test = data["x"][data["k_fold_splits"] == args.split_idx]
+                y_test = data["y"][data["k_fold_splits"] == args.split_idx]
+            else:
+                X_train, X_test, y_train, y_test = train_test_split(
+                    data["x"], data["y"], test_size=0.2, random_state=args.seed
+                )
 
             results = run_model_on_dataset(
                 X_train=X_train,
@@ -329,13 +383,17 @@ def main() -> None:
                 y_test=y_test,
                 model_name=model_name,
                 metrics=args.metrics,
-                optimize_hyperparameters=args.optimize_hyperparameters,
+                evaluation_mode=args.evaluation_mode,
                 n_iter_bayes_opt=args.n_iter_bayes_opt,
+                seed=args.seed,
             )
             results["model"] = model_name
             results["dataset"] = dataset_name
-            full_results.append(results)
+            results["evaluation_mode"] = args.evaluation_mode
+            if args.evaluation_mode == "cross_val":
+                results["split_idx"] = args.split_idx
 
+            full_results.append(results)
             results_string = format_results(model_name, dataset_name, results)
             logger.info(results_string)
 
