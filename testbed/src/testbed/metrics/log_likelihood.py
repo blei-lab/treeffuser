fixed error with parenthesis in the log-likelihood

testbed/src/testbed/metrics/log_likelihood.py
@@ -12,6 +12,9 @@ from tqdm import tqdm
 from testbed.metrics.base_metric import Metric
 from testbed.models.base_model import ProbabilisticModel
 
+# Used for prior  so that 100 samples are enough to get a good estimate
+EPSILON = 1 / 100
+
 
 class LogLikelihoodFromSamplesMetric(Metric):
     """
@@ -112,10 +115,12 @@ def fit_and_evaluate_int_prob(y_train: Float[ndarray, "n_samples y_dim"], y_test
     n_vals = max_y - min_y + 1
 
     unique, counts = np.unique(y_train_int, return_counts=True)
-    total_counts = np.sum(counts) + n_vals
+    total_counts = np.sum(counts) + n_vals * EPSILON
 
     counts_dict = dict(zip(unique, counts))
-    probs_test = np.array([counts_dict.get(y, 0) + 1 / total_counts for y in y_test_int])
+    probs_test = np.array(
+        [(counts_dict.get(y, 0) + EPSILON) / total_counts for y in y_test_int]
+    )
     log_probs_test = np.log(probs_test)
     return np.sum(log_probs_test)
 