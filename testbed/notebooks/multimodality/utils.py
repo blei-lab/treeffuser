ba784a010afdff127c9e84f0b4bfaf9654d65d84
aagrande <47125106+aagrande@users.noreply.github.com>
2024-05-11 22:38:40 -0400

    add mixture of gaussians generator

---
+++ testbed/notebooks/multimodality/utils.py
@@ -0,0 +1,4 @@
+# count the number of modes of a KDE with a given bandwidth h
+# https://github.com/cran/multimode/blob/master/R/mode_testing_lin.R
+# see what happens to the distirbution of h_crit(k) as n->inf
+# silverman (1981)
