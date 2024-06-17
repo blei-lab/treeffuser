cd1ec036e81ec9afa529dfcbf677b57223e97ab1
aagrande <47125106+aagrande@users.noreply.github.com>
2024-06-17 00:56:31 -0400

    replace prediciton plot with scatter plot of raw data vs samples

--- notebooks/README_example.py
+++ notebooks/README_example.py
@@ -4,31 +4,27 @@ import numpy as np
 from treeffuser import Treeffuser
 
 # Generate the data
-rng = np.random.default_rng(seed=0)
+seed = 0
+rng = np.random.default_rng(seed=seed)
 n = 5000
 x = rng.uniform(0, 2 * np.pi, size=n)
-y = np.sin(x) + rng.laplace(scale=x / 20, size=n)
+comp = rng.integers(0, 2, size=n)
+y = comp * np.sin(x - np.pi / 2) + (1 - comp) * np.cos(x) + rng.laplace(scale=x / 20, size=n)
 
 # Fit the model
-model = Treeffuser()
+model = Treeffuser(seed=seed)
 model.fit(x, y)
 
-# Generate samples and return predictions
-x_new = np.linspace(x.min(), x.max(), 200)
-y_samples = model.sample(x_new, n_samples=10**2, verbose=True)
-y_preds = y_samples.mean(axis=0)
-y_q05, y_q95 = np.quantile(y_samples, q=[0.05, 0.95], axis=0)
+# Generate and plot samples
+y_samples = model.sample(x, n_samples=1, seed=seed, verbose=True)
 
-# Plot original data and predictions
-sorted_idx = np.argsort(x_new)
-x_sorted, y_preds_sorted, y_q05_sorted, y_q95_sorted = (
-    arr[sorted_idx] for arr in [x_new, y_preds, y_q05, y_q95]
-)
-plt.plot(x_sorted, y_preds_sorted, color="black")
-plt.fill_between(x_sorted, y_q05_sorted, y_q95_sorted, color="gray", alpha=0.4)
-plt.scatter(x, y, s=1)
+plt.scatter(x, y, s=1, label="raw data")
+plt.scatter(x, y_samples[0, :], s=1, alpha=0.7, label="samples")
+plt.ylim(-2.5, 2.5)
 
-plt.xlabel("x")
-plt.ylabel("y")
+plt.xlabel("$x$")
+plt.ylabel("$y$")
 
+plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.125), ncol=2)
+plt.tight_layout()
 plt.savefig("README_example.png", dpi=120)
