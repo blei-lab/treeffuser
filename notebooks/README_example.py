ab261b1fff629e3768a2bdd3bb4ce1cef2783b4b
aagrande <47125106+aagrande@users.noreply.github.com>
2024-06-17 14:36:28 -0400

    add new example with reduced scale and fit with paper defaults

--- notebooks/README_example.py
+++ notebooks/README_example.py
@@ -5,14 +5,14 @@ from treeffuser import Treeffuser
 
 # Generate the data
 seed = 0
-rng = np.random.default_rng(seed=seed)
 n = 5000
+rng = np.random.default_rng(seed=seed)
 x = rng.uniform(0, 2 * np.pi, size=n)
 comp = rng.integers(0, 2, size=n)
-y = comp * np.sin(x - np.pi / 2) + (1 - comp) * np.cos(x) + rng.laplace(scale=x / 20, size=n)
+y = comp * np.sin(x - np.pi / 2) + (1 - comp) * np.cos(x) + rng.laplace(scale=x / 30, size=n)
 
 # Fit the model
-model = Treeffuser(seed=seed)
+model = Treeffuser(sde_initialize_from_data=True, seed=seed)
 model.fit(x, y)
 
 # Generate and plot samples
@@ -20,11 +20,11 @@ y_samples = model.sample(x, n_samples=1, seed=seed, verbose=True)
 
 plt.scatter(x, y, s=1, label="raw data")
 plt.scatter(x, y_samples[0, :], s=1, alpha=0.7, label="samples")
-plt.ylim(-2.5, 2.5)
 
 plt.xlabel("$x$")
 plt.ylabel("$y$")
 
 plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.125), ncol=2)
 plt.tight_layout()
+
 plt.savefig("README_example.png", dpi=120)
