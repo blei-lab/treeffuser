831796d9ea4386a7af7009162422049334852d5e
Achille Nazaret <aon2108@columbia.edu>
2024-06-25 11:58:24 -0400

    adjust content of tables via js

--- docs/js/tabs.js
+++ docs/js/tabs.js
@@ -1,4 +1,28 @@
 // script.js
+
+// Parse tabcontents and in each cell, make X.xx ± Y.yy to have Y.yy smaller font size
+// This is a hacky way to do it, but it works
+
+document.addEventListener("DOMContentLoaded", function() {
+    var tabcontents = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontents.length; i++) {
+      var tabcontent = tabcontents[i];
+      var cells = tabcontent.getElementsByTagName("td");
+      for (var j = 0; j < cells.length; j++) {
+        var cell = cells[j];
+        var text = cell.innerHTML;
+        var match = text.match(/(\d+\.\d+)±(\d+\.\d+)/);
+        if (match) {
+          var mean = match[1];
+          mean = parseFloat(mean).toFixed(1);
+          var sd = match[2];
+          sd = parseFloat(sd).toFixed(1);
+          cell.innerHTML = mean + "&nbsp;±<span style='font-size: 0.8em;'>" + sd + "</span>";
+        }
+      }
+    }
+    });
+
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
