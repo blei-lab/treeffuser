adc9f4d6443e86c987eb1edd1235feed7c8f9e84
Nicolas Beltran <nb2838@columbia.edu>
2024-08-01 11:21:57 -0400

    added kin8mn preprocess

---
+++ testbed/src/testbed/data/uci/kin8nm/preprocess.py
@@ -0,0 +1,27 @@
+import argparse
+from pathlib import Path
+import numpy as np
+import zipfile
+import pandas as pd
+
+def main(path_raw_dataset_dir: Path):
+    # Process the raw data file named temp.csv
+    raw_data_path = path_raw_dataset_dir / "data.zip"
+    # unzip the data
+    with zipfile.ZipFile(raw_data_path, 'r') as zip_ref:
+        zip_ref.extractall(path_raw_dataset_dir)
+    raw_data_path = path_raw_dataset_dir / "data.csv"
+
+    df = pd.read_csv(raw_data_path)
+    y = df["y"].values
+    x = df.drop(columns=["y"]).values
+    categorical = []
+
+    # Save preprocessed data
+    np.save(path_raw_dataset_dir.parent / "data.npy", {"x": x, "y": y, "categorical": categorical})
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=Path)
+    args = parser.parse_args()
+    main(args.path)
