1a20ec682e41738a32688f7b67b5f575dc429ce7
Nicolas Beltran <nb2838@columbia.edu>
2024-10-16 16:52:26 -0400

    ran pre-commit

--- testbed/src/testbed/data/uci/kin8nm/preprocess.py
+++ testbed/src/testbed/data/uci/kin8nm/preprocess.py
@@ -1,14 +1,16 @@
 import argparse
-from pathlib import Path
-import numpy as np
 import zipfile
+from pathlib import Path
+
+import numpy as np
 import pandas as pd
+
 
 def main(path_raw_dataset_dir: Path):
     # Process the raw data file named temp.csv
     raw_data_path = path_raw_dataset_dir / "data.zip"
     # unzip the data
-    with zipfile.ZipFile(raw_data_path, 'r') as zip_ref:
+    with zipfile.ZipFile(raw_data_path, "r") as zip_ref:
         zip_ref.extractall(path_raw_dataset_dir)
     raw_data_path = path_raw_dataset_dir / "data.csv"
 
@@ -18,7 +20,10 @@ def main(path_raw_dataset_dir: Path):
     categorical = []
 
     # Save preprocessed data
-    np.save(path_raw_dataset_dir.parent / "data.npy", {"x": x, "y": y, "categorical": categorical})
+    np.save(
+        path_raw_dataset_dir.parent / "data.npy", {"x": x, "y": y, "categorical": categorical}
+    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
